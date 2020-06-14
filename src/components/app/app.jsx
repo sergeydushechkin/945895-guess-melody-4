c36fd23dbd31d63cfa24ebcd3096e52fe9dbb7c0
Sergey <54816584+sergeydushechkin@users.noreply.github.com>
2020-06-14 20:25:15 +0300

    Устанавливает prop-types и добавляет проверки

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {errorsCount} = props;
 
   return (
     <WelcomeScreen errorsCount = {errorsCount}/>
   );
+};
+
+App.propTypes = {
+  errorsCount: PropTypes.number.isRequired
 };
 
 export default App;
