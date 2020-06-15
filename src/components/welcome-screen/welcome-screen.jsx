f2a5b90826abe8621f783ffdd994218338407d13
Sergey <dushechkin_serge@mail.ru>
2020-06-15 12:27:27 +0300

    Обновляет снепшот тесты. Добавит обработчик кнопки.

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const WelcomeScreen = (props) => {
-  const {errorsCount} = props;
+  const {errorsCount, onWelcomeButtonClick} = props;
 
   return (
     <section className="welcome">
       <div className="welcome__logo">
         <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
       </div>
-      <button className="welcome__button"><span className="visually-hidden">Начать игру</span></button>
+      <button
+        onClick={onWelcomeButtonClick}
+        className="welcome__button"
+      >
+        <span className="visually-hidden">Начать игру</span>
+      </button>
       <h2 className="welcome__rules-title">Правила игры</h2>
       <p className="welcome__text">Правила просты:</p>
       <ul className="welcome__rules-list">
@@ -22,7 +27,8 @@ const WelcomeScreen = (props) => {
 };
 
 WelcomeScreen.propTypes = {
-  errorsCount: PropTypes.number.isRequired
+  errorsCount: PropTypes.number.isRequired,
+  onWelcomeButtonClick: PropTypes.func.isRequired
 };
 
 export default WelcomeScreen;
